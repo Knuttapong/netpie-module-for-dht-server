a4b6187fcb41c28b2a79fa1fd649efa98ab890e8
knuttapong <harenalas@gmail.com>
2017-07-20 11:14:14 +0700

    code tab edit

--- netpie.js
+++ netpie.js
@@ -9,8 +9,8 @@ var microgear = MicroGear.create({
 });
 
 exports.timeserie = [
-  {"target": "temp", "datapoints": [ ]},
-  {"target": "humid", "datapoints": [ ]}
+  	{"target": "temp", "datapoints": [ ]},
+  	{"target": "humid", "datapoints": [ ]}
 ];
 
 var temp = 0;
@@ -23,18 +23,18 @@ microgear.on('connected', function() {
 	microgear.subscribe("/dht");
 
 	setInterval(function() {
-    now = Date.now();
-    exports.timeserie[0].datapoints[count] = [temp, now];
-    exports.timeserie[1].datapoints[count] = [humid, now];
-    count++;
+    		now = Date.now();
+    		exports.timeserie[0].datapoints[count] = [temp, now];
+    		exports.timeserie[1].datapoints[count] = [humid, now];
+    		count++;
 	},5000);
 });
 
 microgear.on('message', function(topic,body) {
-  dht = body+"";
-  data = dht.split(",");
-  temp = data[1];
-  humid = data[0];
+  	dht = body+"";
+  	data = dht.split(",");
+  	temp = data[1];
+  	humid = data[0];
 });
 
 microgear.on('closed', function() {
